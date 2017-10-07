50debd31786ffb1c6894c5f9a4ebc9434562c333
Fujitechs <fujitechs416@gmail.com>
2017-10-07 00:57:30 -0700

    Added counter. Forces string lowercase. Trims out retweets

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import tweepy
 
+## 15 CALLS EVERY 15 MINUTES. Running program too many times will give status code 429
+
 
 # sets up auth and access keys
 consumer_key = '858dJ9NBYSMgu2u7Onu6LBcbG'
@@ -15,28 +17,27 @@ auth1.set_access_token(access_token_key, access_token_secret)
 api = tweepy.API(auth1)
 
 handle = input("Input Twitter Handle: ")
+keywrd = input("Please enter a search term: ")
 user = api.get_user(handle)
 
-result = []
-for status in tweepy.Cursor(api.user_timeline, id =handle).items(10):
-	result.append(status.text)
-	#print ("\n{}".format(status.text))
-#print("{}".format(result))
+count = 0
+for status in tweepy.Cursor(api.user_timeline, id = handle).items(100):
+	if "RT" in status.text:
+		continue
 
-keywrd = input("Please enter a search term: ")
-count=0
-for x in result[:]:
-	if x == keywrd:
-		count += 1
-		print ("{}".format(x))
+	#Use below to print to text file
+	#with open("Output.txt", "a") as text_file:
+		#print("{}\n".format(str.lower(status.text)), file=text_file)	
 
-# print("\nTwitter Handle: {}".format(user.screen_name))
-# print("\nName: {}".format(user.name))
-# print("Description: {}".format(user.description))
-# print("Followers: {}".format(user.followers_count))
-# print("Friends: {}".format(user.friends_count))
+	if keywrd in str.lower(status.text):
+		count+=1
+
+print("\nName: {}".format(user.name))
+print("Description: {}".format(user.description))
+print("Followers: {}".format(user.followers_count))
+print("Friends: {}".format(user.friends_count))
+print("# of hits for {}: {}".format(keywrd,count))
+	
 
 
-# Streaming tweets, not used?
-#myStream = tweepy.Stream(auth = api.auth, listener=MyStreamListener())
-#myStream.filter(track=['python'], async=True)
+	
